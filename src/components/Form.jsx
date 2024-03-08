0d41a8bafdf4ec06b846704ee14785d39ba10135
Anthony Arango <anthonyarango01@gmail.com>
2024-03-07 19:41:47 -0500

    CHG-Se agregó a la funcionalidadde buscar la validación de campo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react"
 
 
-export const Form = () => {
+export const Form = ({ setCategory }) => {
     const [value, setValue] = useState("");
+    const [error, setError] = useState(false);
 
+    const searchGif = e=>{
+        e.preventDefault();
+        if(value === ""){
+            setError(true);
+            return;
+        }
 
-  return (
+        console.log("enter form")
+    }
+
+  return (  //Aquí estamos recolectando el valor digitado por el usuario, cambiando su estado y ejecutando el metodo searchGif
+
     <section className="form">
         {value}
-        <form action="">
+        <form onSubmit={searchGif}>
             
             <input type="text" placeholder="Digita la categoria"
             onChange={e=>setValue(e.target.value)}/>
         </form>
+        {error ? <p className="error">El campo no puede estar vacio </p>:""} 
+        
     </section>
   )
 }
