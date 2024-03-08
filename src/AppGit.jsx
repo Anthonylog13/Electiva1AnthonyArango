e9a144d97357a6dc45d072defb95bb6c2c5089f5
Anthony Arango <anthonyarango01@gmail.com>
2024-03-07 20:38:18 -0500

    ADD- Endpoint listo para hacer la llamada

--- src/AppGit.jsx
+++ src/AppGit.jsx
@@ -1,14 +1,19 @@
 import {HHeader} from "./components/HHeader"
 import {Footer} from "./components/Footer"
 import { Form } from "./components/Form"
+import { useState } from "react"
+import { SeeGifss } from "./components/SeeGifss";
 
 function AppGit(){
+  const [category, setCategory]= useState(0);
   return(
     <div className="App">
       <HHeader/>
-      <Form/>
+      <Form setCategory={setCategory}/>
+      <SeeGifss category={category}/>
       
       <h2>main</h2>
+      
       <Footer/>
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,18 +11,20 @@ export const Form = ({ setCategory }) => {
             setError(true);
             return;
         }
-
-        console.log("enter form")
+        setError(false);
+        setCategory(value);
+        setValue("");
     }
 
   return (  //Aquí estamos recolectando el valor digitado por el usuario, cambiando su estado y ejecutando el metodo searchGif
 
     <section className="form">
-        {value}
+        
         <form onSubmit={searchGif}>
             
             <input type="text" placeholder="Digita la categoria"
-            onChange={e=>setValue(e.target.value)}/>
+            onChange={e=>setValue(e.target.value)} value ={value}/>
+            
         </form>
         {error ? <p className="error">El campo no puede estar vacio </p>:""} 
         
